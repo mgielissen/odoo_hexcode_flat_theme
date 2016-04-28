ef7909790a9ab02b49f778a67b2f494d76f39f59
FedericoRanieri <federico.ranieri96@gmail.com>
2016-04-28 14:28:48 +0200

    first commit

---
+++ hexcode_flat_theme/__openerp__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Flat Theme',
+    'version': '1.0',
+    'category': 'theme',
+    'description': '',
+    'author': 'Federico Ranieri',
+    'website': 'www.federicoranieri.it',
+    'depends': [],
+    'data': [
+        'views/load.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'price': 89,
+    'currency': 'EUR',
+    'images': ['images/main_screenshot.png'],
+}
